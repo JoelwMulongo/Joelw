4739092f78bb261d7550af014142d7adc3d3b814
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-20 03:43:45 +0300

    Create Navigation.dart

---
+++ flutter/Navigation.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Go to SecondScreen'),
+        onPressed: () => Navigator.pushNamed(context, '/second'),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  void _pushSecondScreen(context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          child: Text('Go back!'),
+          onPressed: () => Navigator.pop(context),
+        ),
+        RaisedButton(
+          child: Text('Go to SecondScreen... again!'),
+          onPressed: () => _pushSecondScreen(context),
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => FirstScreen(),
+      '/second': (context) => SecondScreen(),
+    },
+  ));
+}
