82e7fdd8c0ed186f04fb1e3911e4b469f2cb2f6a
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-20 03:40:04 +0300

    Create ShowAlert.dart

---
+++ flutter/ShowAlert.dart
@@ -0,0 +1,33 @@
+showDialog<void>(
+  context: context,
+  barrierDismissible: false,
+  builder: (BuildContext context) {
+    return AlertDialog(
+      title: Text('Alert Title'),
+      content: Text('My Alert Msg'),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Ask me later'),
+          onPressed: () {
+            print('Ask me later pressed');
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('Cancel'),
+          onPressed: () {
+            print('Cancel pressed');
+            Navigator.of(context).pop();
+          },
+        ),
+        FlatButton(
+          child: Text('OK'),
+          onPressed: () {
+            print('OK pressed');
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  },
+);
