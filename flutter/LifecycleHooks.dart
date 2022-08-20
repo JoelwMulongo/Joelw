2337e94dd7f1933ae08c834d45585815ed48d4f4
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-08-20 03:50:27 +0300

    Create LifecycleHooks.dart

---
+++ flutter/LifecycleHooks.dart
@@ -0,0 +1,25 @@
+class _MyComponentState extends State<MyComponent> {
+  @override
+  void initState() {
+    // this method is called before the first build
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(MyComponent oldWidget) {
+    // this method IS called when parent widget is rebuilt
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override didChangeDependencies() {
+    // called when InheritedWidget updates
+    // read more here https://api.flutter.dev/flutter/widgets/InheritedWidget-class.html
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    // called after widget was unmounted from widget tree
+    super.dispose();
+  }
+}
